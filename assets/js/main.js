ca5da0a126335281882c3adf8afa4003f8d7f83f
Molnár János <janos.molnar828@gmail.com>
2026-01-23 09:13:02 +0100

    header js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,29 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
+if (navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+if (navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== HOME TYPED JS ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
-
+const LinkAction = ()=>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', LinkAction))
 /*=============== ADD SHADOW HEADER ===============*/
 
 
